834f27e76e38bd3366fc5a4d8b45b956a8e381ea
Edward Yang <Edward_Yang@asus.com>
2021-10-15 14:30:48 +0800

    mdy routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "module-a", loadChildren: "./module-a/module-a.module#ModuleAModule" },
-  { path: "module-b", loadChildren: "./module-b/module-b.module#ModuleBModule" }
+  {
+    path: 'module-a',
+    loadChildren: () => import('./module-a/module-a.module').then(m => m.ModuleAModule)
+  }, {
+    path: 'module-b',
+    loadChildren: () => import('./module-b/module-b.module').then(m => m.ModuleBModule)
+  }
 ];
 
 @NgModule({
