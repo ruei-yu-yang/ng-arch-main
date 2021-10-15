4f3c4feed11dd007305d5f411374e7a16e45bff5
Edward Yang <Edward_Yang@asus.com>
2021-10-15 14:05:11 +0800

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "module-a", loadChildren: "./module-a/module-a.module#ModuleAModule" },
+  { path: "module-b", loadChildren: "./module-b/module-b.module#ModuleBModule" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
